057e2e6cb69c4aba2c96bd6fc357cb6b55140965
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-05-03 18:32:44 +0200

    setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export  function App() {
+  return (
+    <div className="flex h-screen flex-col justify-center items-center p-4 ">
+      <h1 className=" mx-auto my-auto text-3xl font-bold ">Welcome</h1>
+    </div>
+  );
+}
+export default App
